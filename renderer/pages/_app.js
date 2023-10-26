7d420487a93e47204e6d907dba04277aae9aeb36
arisia <starlights2283@gmail.com>
2023-10-26 22:37:29 +0900

    first commit

---
+++ renderer/pages/_app.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import Link from 'next/link'
+import './globals.css'
+import 'antd/dist/antd.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <React.Fragment>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
+}
+
+export default MyApp
---
+++ renderer/pages/home.js
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <div className="test">Hallo</div>
+    )
+}
