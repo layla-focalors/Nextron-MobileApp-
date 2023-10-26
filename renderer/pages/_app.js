3c5609b47ebff4e4ba68a96b7de9db58ee2817fb
arisia <starlights2283@gmail.com>
2023-10-26 23:09:37 +0900

    Add update

--- renderer/pages/_app.js
+++ renderer/pages/_app.js
@@ -1,20 +1,5 @@
-import React from 'react'
-import Head from 'next/head'
-import { AppProps } from 'next/app'
-import Link from 'next/link'
-import './globals.css'
-import 'antd/dist/antd.css'
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <React.Fragment>
-      <Head>
-        <title>DeepDynamic PCM</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-    </React.Fragment>
-  )
+export default function App(){
+    return (
+        <div className="test">8</div>
+    )
 }
-
-export default MyApp
